ceaf82d8bca2179684e05522d97b64d713159dbf
juanhrnandez <75130031+juanhrnandez@users.noreply.github.com>
2023-02-26 15:44:56 -0600

    Update HorizontalGallery.js
    Now you can simply add and change the images in one array. 
    
    I remove some classes that are not necessary to work.

--- components/HorizontalGallery.js
+++ components/HorizontalGallery.js
@@ -1,21 +1,40 @@
-import { useEffect, useRef } from 'react';
-import Image from 'next/image';
-import gsap from 'gsap';
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import gsap from "gsap";
 
-import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+//now you can use any images you want with the same structure
+const Images = [
+  {
+    src: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "1st image",
+  },
+  {
+    src: "https://images.pexels.com/photos/2902536/pexels-photo-2902536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "2ndimage",
+  },
+  {
+    src: "https://images.pexels.com/photos/1089194/pexels-photo-1089194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "3ndimage",
+  },
+  {
+    src: "https://images.pexels.com/photos/1601775/pexels-photo-1601775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "4ndimage",
+  },
+];
 
 gsap.registerPlugin(ScrollTrigger);
 
-function App() {
+export default function HorizontalGallery() {
   const scroller = useRef();
   const skills = useRef();
 
   useEffect(() => {
-    let skillSet = gsap.utils.toArray('.skill-set');
-
+    let skillSet = gsap.utils.toArray(".skill-set");
     let to = gsap.to(skillSet, {
       xPercent: () => -100 * (skillSet.length - 1),
-      ease: 'none',
+      ease: "none",
       scrollTrigger: {
         trigger: scroller.current,
         markers: false,
@@ -25,76 +44,37 @@ function App() {
         invalidateOnRefresh: true,
         anticipatePin: 1,
         snap: 1 / (skillSet.length - 1),
-
-        end: () => '+=' + window.innerWidth,
+        end: () => "+=" + window.innerWidth,
       },
     });
-
     return () => {
       to.kill();
     };
   }, []);
 
   return (
-    <div className="overflow-hidden flex">
-      <div className="overflow-hidden ">
+    <div className="overflow-hidden">
         <div
           id="skills"
           ref={scroller}
           className=" flex overflow-x-hidden text-white w-[400vw] m-0 bg-gray-900  relative h-screen"
         >
-          <section
-            ref={skills}
-            className="skill-set px-12 w-screen h-full bg-transparent ns-horizontal-section__item flex items-center z-50"
-          >
-            <Image
-              src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="1st image"
-              layout="fill"
-              objectFit="contain"
-              className="max-w-[70vw] max-h-[60vh] m-auto"
-            />
-          </section>
-          <section
-            ref={skills}
-            className="skill-set px-12 w-screen h-full bg-transparent ns-horizontal-section__item flex items-center z-50"
-          >
-            <Image
-              src="https://images.pexels.com/photos/2902536/pexels-photo-2902536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="2ndimage"
-              layout="fill"
-              objectFit="contain"
-              className="max-w-[70vw] max-h-[60vh] m-auto"
-            />
-          </section>
-          <section
-            ref={skills}
-            className="skill-set px-12 w-screen h-full bg-transparent ns-horizontal-section__item flex items-center z-50"
-          >
-            <Image
-              src="https://images.pexels.com/photos/1089194/pexels-photo-1089194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="2ndimage"
-              layout="fill"
-              objectFit="contain"
-              className="max-w-[70vw] max-h-[60vh] m-auto"
-            />
-          </section>
-          <section
-            ref={skills}
-            className="skill-set px-12 w-screen h-full bg-transparent ns-horizontal-section__item flex items-center z-50"
-          >
-            <Image
-              src="https://images.pexels.com/photos/1601775/pexels-photo-1601775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="2ndimage"
-              layout="fill"
-              objectFit="contain"
-              className="max-w-[70vw] max-h-[60vh] m-auto"
-            />
-          </section>
-        </div>
+          {Images.map((image, index) => (
+            <section
+              ref={skills}
+              key={image.alt}
+              className="skill-set px-12 w-screen h-full bg-transparent ns-horizontal-section__item flex items-center z-50"
+            >
+              <Image
+                src={image.src}
+                alt={image.alt}
+                layout="fill"
+                objectFit="contain"
+                className="max-w-[70vw] max-h-[60vh] m-auto"
+              />
+            </section>
+          ))}
       </div>
     </div>
   );
 }
-
-export default App;
